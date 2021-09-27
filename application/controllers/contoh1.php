aae493deb9e8bdec6aa37537abd4bfd14cac6eda
gumelarseptiawan <gumelarseptiawan0@gmail.com>
2021-09-27 09:35:10 +0700

    commite pertama

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama Saya Gumelar Septiawan<br>
+           Saya tinggal di daerah Asrama Zeni TNI AD Jakarta Timur<br>
+           Hobi saya bermain futsal dan berenang"
+    }
+}
+    
